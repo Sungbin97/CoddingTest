40bc784ce765e4d2265d47c22791adedac02281a
Administrator <csb3694@naver.com>
2022-11-15 10:57:37 +0900

    ReportResult
    컴파일 에러 해결 중

--- src/programmers/level1/kakao/ReportResult.java
+++ src/programmers/level1/kakao/ReportResult.java
@@ -1,6 +1,7 @@
 package programmers.level1.kakao;
 
 import java.util.*;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ReportResult {
@@ -12,58 +13,56 @@ public class ReportResult {
         List<String> reporter = new ArrayList<>();
         List <String> reported = new ArrayList<>();
 
-        for (String id : id_list) map.put(id, "");
+        List<String> reportDistinct = Arrays.stream(report).distinct().collect(Collectors.toList()); // 중복된 신고 제거
 
-        for (int i = 0; i < report.length; i++) {
-            reporter.add(report[i].substring(0, report[i].indexOf(" ")));
-            reported.add(report[i].substring(report[i].indexOf(" ")).replaceAll(" ", ""));
+        for (String id : id_list) map.put(id, ""); // 신고자를 key로 저장
+
+        for (int i = 0; i < reportDistinct.size(); i++) { // 중복된 신고를 제외한 List를 기준으로
+                                                          // 신고자 + 피신고자 문자열을 신고자와 피신고자 List로 분할
+            reporter.add(reportDistinct.get(i).substring(0, report[i].indexOf(" ")));
+            reported.add(reportDistinct.get(i).substring(report[i].indexOf(" ")).replaceAll(" ", ""));
         }
         System.out.println("reporter: " + reporter);
         System.out.println("reported: " + reported);
 
-        List<String> banList = new ArrayList<>(); // k번 이상 신고된 사람
-        Set<String> set = new HashSet<>(reported);
-        for (String str : set) {
+        List<String> banList = new ArrayList<>(); // k번 이상 신고된 사람을 저장할 List
+        Set<String> set = new HashSet<>(reported); // 신고된 사람을 중복없이 저장할 Set
+
+        for (String str : set) { // k번 이상 신고된 사람을 banList에 추가
             if(Collections.frequency(reported, str) >= k)
                 banList.add(str);
         }
         System.out.println("banList" + banList);
-
-        for (int i = 0; i < reporter.size(); i++){
+        
+        for (int i = 0; i < reporter.size(); i++){ // 신고인(문자열)과 피신고인(문자열)을 map에 저장
             map.put(reporter.get(i)
                     , map.get(reporter.get(i)) + " " + reported.get(i));
         }
-        System.out.println(map);
+        System.out.println("map: " + map);
 
-        for (int i = 0; i < map.size(); i++) {
+        for (int i = 0; i < map.size(); i++) { // 신고인(문자열)과 피신고인(리스트)을 map2에 저장
             map2.put(id_list[i],
-                    Arrays.stream(map
-                            .get(id_list[i])
-                            .replaceFirst(" ", "")
-                            .split(" "))
-                            .filter(j -> j != "")
+                    Arrays.stream(
+                                    map
+                                            .get(id_list[i])
+                                            .replaceFirst(" ", "")
+                                            .split(" "))
+                            .filter(j -> j != ""
+                                    && banList.stream().anyMatch(Predicate.isEqual(j))) //정지당한 사람만 저장
                             .collect(Collectors.toList()));
         }
-        System.out.println(map2);
-        List<List> lists = new ArrayList<>();
-        for (int i = 0; i < map.size(); i++) {
-            lists.add(
-                    Arrays.stream(map
-                                    .get(id_list[i])
-                                    .replaceFirst(" ", "")
-                                    .split(" "))
-                                    .filter(j -> j != "")
-                                    .collect(Collectors.toList())
-            );
-        }
-        System.out.println("lists: " + lists);
 
+        System.out.println("map2: " + map2);
+        for (int i = 0; i < id_list.length; i++) answer[i] = map2.get(id_list[i]).size(); // 신고자가 신고하여 정지당한 수를 구하여 answer에 저장
+        for (int i : answer) System.out.println("answer: " + i);
 
         return answer;
     }
     public static void main(String[] args) {
+        /*String[] id_list = {"con", "ryan"};
+        String[] report = {"ryan con", "ryan con", "ryan con", "ryan con"};*/
         String[] id_list = {"muzi", "frodo", "apeach", "neo"};
-        String[] report = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
+        String[] report = {"muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"};
         int[] answer = {};
         solution(id_list, report, 2);
     }
